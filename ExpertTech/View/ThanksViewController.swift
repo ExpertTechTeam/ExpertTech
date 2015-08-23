e23810300f1cc168bfa170ba53ae1abcc8fe81be
ninguchi <kewalins@Kewalins-MacBook-Pro.local>
2015-08-23 21:48:06 +0700

    Change bg color

--- ExpertTech/View/ThanksViewController.swift
+++ ExpertTech/View/ThanksViewController.swift
@@ -13,7 +13,6 @@ class ThanksViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.hidden = false
-        self.view.backgroundColor = Constants.Color.LightGray
         self.navigationItem.setHidesBackButton(isHiddenBackBtn, animated: false)
         // Do any additional setup after loading the view.
     }
