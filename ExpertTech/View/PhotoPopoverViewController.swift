14d906d370ab2c0789d4d66b07f2a72b23826159
ninguchi <kewalins@Kewalins-MacBook-Pro.local>
2015-08-25 15:56:14 +0700

    Photo Popover

--- ExpertTech/View/PhotoPopoverViewController.swift
+++ ExpertTech/View/PhotoPopoverViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoPopoverViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var vCollectionView: UICollectionView!
+    var images:NSMutableArray = NSMutableArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(animated: Bool) {
+        self.fetchPhotoAtIndexFromEnd(0)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +33,81 @@ class PhotoPopoverViewController: UIViewController, UICollectionViewDelegate, UI
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+
+            return images.count
+
+        
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
-        cell.backgroundColor = Constants.Color.LightGray_BG
-        // Configure the cell
+        let imageView:UIImageView = UIImageView(image: images[indexPath.row]  as? UIImage)//UIImageView(image: UIImage(named: "photo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        cell.addSubview(imageView)
+        if imageView.frame.width>imageView.frame.height {
+        let constraintWidth = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0.0)
+            cell.addConstraint(constraintWidth)
+        }
+        if imageView.frame.width<=imageView.frame.height {
+            let constraintHeight = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Height, multiplier: 1, constant: 0.0)
+            cell.addConstraint(constraintHeight)
+        }
+        let constraintVertical = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
+        let constraintHorizontal = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0)
+        
+        
+        cell.addConstraint(constraintVertical)
+        cell.addConstraint(constraintHorizontal)
+
         return cell
+    }
+    func fetchPhotoAtIndexFromEnd(index:Int) {
+        
+        let imgManager = PHImageManager.defaultManager()
+        
+        // Note that if the request is not set to synchronous
+        // the requestImageForAsset will return both the image
+        // and thumbnail; by setting synchronous to true it
+        // will return just the thumbnail
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.synchronous = true
+        let fetchOptions = PHFetchOptions()
+            fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: true)]
+            
+            let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
+            
+            // If the fetch result isn't empty,
+            // proceed with the image request
+            if fetchResult.count > 0 {
+                // Perform the image request
+                imgManager.requestImageForAsset(fetchResult.objectAtIndex(fetchResult.count - 1 - index) as! PHAsset, targetSize: view.frame.size, contentMode: PHImageContentMode.AspectFill, options: requestOptions, resultHandler: { (image, _) in
+                    
+                    // Add the returned image to your array
+                    self.images.addObject(image!)
+                    //let curImage:UIImage = image!
+                    //curImage.images.
+                    
+                    
+                    // If you haven't already reached the first
+                    // index of the fetch result and if you haven't
+                    // already stored all of the images you need,
+                    // perform the fetch request again with an
+                    // incremented index
+                    if index + 1 < fetchResult.count {//&& self.images.count < self.totalImageCountNeeded {
+                        self.fetchPhotoAtIndexFromEnd(index + 1)
+                    } else {
+                        // Else you have completed creating your array
+                        print("Completed array: \(self.images), Count : \(self.images.count)" )
+                        self.vCollectionView.reloadData()
+                    }
+                })
+            }
+
+//}
+       // PHPhotoLibrary authorizationStatus] – if not set, it will return PHAuthorizationStatusNotDetermined
+        // Sort the images by creation date
+        
     }
 
     /*
